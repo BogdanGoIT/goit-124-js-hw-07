6d186254b80de45f35a399d3b3d68623788aae54
Dictator <dictatorbogdan@gmail.com>
2026-01-17 12:47:58 +0200

    add js-hw7 task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const email = evt.target.elements.email.value;
+  const password = evt.target.elements.password.value;
+
+  if (email.trim() && password.trim()) {
+    const obj = { email, password };
+    console.log(obj);
+    form.reset();
+  } else {
+    alert('All form fields must be filled in');
+  }
+});
